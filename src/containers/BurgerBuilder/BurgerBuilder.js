5b7a453d0d329a9b722eb0e4b2c4304bfc9367ba
steven-espin <sespin.developer@gmail.com>
2018-09-10 17:14:14 -0700

    added button function to add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import AuxComponent from '../../hoc/AuxComponent';
 import Burger from '../../components/Burger/Burger';
 import BurgerControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENTS_PRICES = {
+    salad: .50,
+    bacon: 1.50, 
+    cheese: 1.00,
+    meat: 1.50
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -11,14 +18,27 @@ class BurgerBuilder extends Component {
             bacon: 0, 
             cheese: 0, 
             meat: 0
+        }, 
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const updatedCount = this.state.ingredients[type] + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
         }
+        updatedIngredients[type] = updatedCount;
+
+        const newPrice = this.state.totalPrice + INGREDIENTS_PRICES[type];
+
+        this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
     }
 
     render() {
         return (
             <AuxComponent>
                 <Burger ingredients={this.state.ingredients}/>
-                <BurgerControls/>
+                <BurgerControls ingredientAdded={this.addIngredientHandler}/>
             </AuxComponent>
         );
     }
