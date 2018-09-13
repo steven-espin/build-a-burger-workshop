9561942c203c8f9e4fd9f27e0c3ee7749d410004
steven-espin <sespin.developer@gmail.com>
2018-09-13 12:50:55 -0700

    Added logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import AuxComponent from '../../hoc/AuxComponent';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = (props) => (
     <AuxComponent>
-        <div>ToolBar, SideDrawer, Backdrop</div>
+        <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
